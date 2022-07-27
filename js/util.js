eb6faebe3d6556779956d2b6996961b286f2204b
Зюзюкина Екатерина <katiezuzukina@mail.ru>
2022-07-27 19:30:51 +0300

    Добавляет надо подкачаться

--- js/util.js
+++ js/util.js
@@ -14,4 +14,30 @@ function checkStringLength (string, maxString = 140) {
 //кнопка клавиатуры esc
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {getRandomIntInclusive, checkStringLength, isEscapeKey};
+const showMessageError = (message) => {
+  const messageBlock = document.createElement('div');
+  messageBlock.style.zIndex = '100';
+  messageBlock.style.position = 'absolute';
+  messageBlock.style.minHeight = '100px';
+  messageBlock.style.border = 'dashed 2px #000';
+  messageBlock.style.opacity = '0.7';
+  messageBlock.style.width = '50%';
+  messageBlock.style.margin = '0 auto';
+  messageBlock.style.left = '0';
+  messageBlock.style.right = '0';
+  messageBlock.style.top = '0';
+  messageBlock.style.padding = '30px 10px';
+  messageBlock.style.fontSize = '20px';
+  messageBlock.style.textAlign = 'center';
+  messageBlock.style.color = '#000';
+  messageBlock.style.backgroundColor = '#ffffff';
+
+  messageBlock.textContent = message;
+  document.body.append(messageBlock);
+
+  setTimeout(() => {
+    messageBlock.remove();
+  }, 5000);
+};
+
+export {getRandomIntInclusive, checkStringLength, isEscapeKey, showMessageError};
